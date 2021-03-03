654616ca9ef721c65b77f0541fa13a896b3379d7
QWR3 <anton7@i.ua>
2021-03-03 17:41:58 +0200

    Home work 3

--- main.py
+++ main.py
@@ -1,194 +1,75 @@
-# strings
-#
-# 1)написати прогу яка вибирає зі введеної строки цифри і виводить їх через кому,
-# наприклад:
-# st = 'as 23 fdfdg544' введена строка
-# 2,3,5,4,4        #вивело в консолі.
-
-
-# st = input('string')
-# l =[]
-# i= 0
-# while i < len(st):
-#     if st[i:i+1].isdigit():
-#         l.append(st[i:i+1])
-#     i = i+1
-# print(l)
-
-
-# #################################################################################
-# 2)написати прогу яка вибирає зі введеної строки числа і виводить їх
-# так як вони написані
-# наприклад:
-#   st = 'as 23 fdfdg544 34' #введена строка
-#   23, 544, 34              #вивело в консолі
-
-# st = input('string:')
-# l = ''
-# for item in st:
-#     if item.isdecimal():
-#         l+=item
-#     else:
-#         l+=' '
-#
-# jj = ','.join(l.split())
-# print(jj)
-#
-
-
-# #################################################################################
-#
-# list comprehension
-#
-# 1)есть строка:
-# greeting = 'Hello, world'
-# записать каждый символ в лист поменяв его на верхний регистр
-# пример:
-# ['H', 'E', 'L', 'L', 'O', ',', ' ', 'W', 'O', 'R', 'L', 'D']
-
-# l ='Hello, world'
-# i = 0
-# li = []
-# while i < len(l):
-#     li.append(l[i].upper())
-#     i = i+1
-# print(li)
-
-#
-# 2) с диапазона от 0-50 записать в лист только не парные числа, при этом возвести их в квадрат
-# пример:
-# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, ...]
-
-# i= 0
-# l =[]
-# while i < 50:
-#     if i % 2 != 0:
-#         l.append(i**2   )
-#     i =i+1
-# print(l)
-
-
-# function
-#
-# - створити функцію яка виводить ліст
-# def function():
-#     s = input('введіть щось:')
-#     ns = s.split(' ')
-#     print(ns)
-# function()
-# - створити функцію яка приймає три числа та виводить та повертає найменьше.
-
-
-# def function():
-#     a = float(input('first number : '))
-#     b = float(input('second number : '))
-#     c = float(input('third number : '))
-#     l = [a,b,c]
-#     print(round(min(l)))
-#     return round(min(l))
-#
-# function()
-
-# - створити функцію яка приймає три числа та виводить та повертає найбільше.
-
-# def function():
-#     a = float(input('first number : '))
-#     b = float(input('second number : '))
-#     c = float(input('third number : '))
-#     l = [a,b,c]
-#     print(round(max(l)))
-#     return round(max(l))
-# function()
-
-# - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
-
-# def function():
-#     s = input('введіть числа:')
-#     ns = s.split(' ')
-#     l = []
-#     i = 0
-#     while i< len(ns):
-#         l.append(int(ns[i]))
-#         i +=1
-#     print(max(l))
-#     return min(l)
-#
-# f = function()
-
-
-# - створити функцію яка виводить ліст
-
-# def function():
-#     s = input('введіть щось:')
-#     ns = s.split(' ')
-#     print(ns)
-# function()
-
-# - створити функцію яка повертає найбільше число з ліста
-# def function():
-#     s = input('введіть щось:')
-#     ns = s.split(' ')
-#     l = []
-#     i = 0
-#     while i < len(ns):
-#         if ns[i].isdigit():
-#             l.append(int(ns[i]))
-#         i +=1
-#     print(max(l))
-#     return max(l)
-# function()
-
-# - створити функцію яка повертає найменьше число з ліста
-# def function():
-#     s = input('введіть щось:')
-#     ns = s.split(' ')
-#     l = []
-#     i = 0
-#     while i < len(ns):
-#         if ns[i].isdigit():
-#             l.append(int(ns[i]))
-#         i +=1
-#     print(min(l))
-#     return min(l)
-# function()
-# - створити функцію яка приймає ліст чисел та складає значення елементів ліста та повертає його.
-# def function():
-#     s =input('введіть щось:')
-#     l = s.split(' ')
-#     n = 0
-#     for i in l:
-#         n +=int(i)
-#     print(n)
-#     return n
-# function()
-# - створити функцію яка приймає ліст чисел та повертає середнє арифметичне його значень.\
-# def function():
-#     s =input('введіть щось:')
-#     l = s.split(' ')
-#     n = 0
-#     for i in l:
-#         n +=int(i)
-#     result =round(n/len(l))
-#     print(result)
-#     return result
-# function()
-#
-# decorators
-# - є функція:
-# def pr():
-#     return 'Hello_boss_!!!'
-#  написати декоратор до цієї функції, який замінює нижні підчеркування на пробіли і повертає це значення
-
-# def decor(func):
-#     def wrap():
-#         # func.index('_')
-#         return func().replace('_', ' ')
-#     return wrap()
+# 1)
+# создать класс Human(имя и возраст)
+# и два класса Prince и Cinderella которые наследуются от Human
+# у принца должен быть размер туфельки и  метод который принимает лист золушек и выводит какой золушки подошла туфелька
+# class Human:
+#     def __init__(self, age, name, size):
+#         self.age = age
+#         self.name = name
+#         self.size = size
 #
 #
-# @decor
-# def task():
-#     return 'Hello_boss_!!!'
+# class Prince(Human):
+#     def __init__(self, age, name, size, list):
+#         super().__init__(age, name, size)
+#         self.list = list
+#         for i in self.list:
+#             if i['size'] == size:
+#                 print(i['name'])
 #
 #
-# print(task)
+# class Cinderella(Human):
+#     def __init__(self, age, name, size):
+#         super().__init__(age, name, size)
+#
+#
+# cinderella = Cinderella(14, 'cinderella', 38)
+# fake_cinderella = Cinderella(34, 'fake', 43)
+# fake_cinderella2 = Cinderella(32, 'fake', 42)
+# prince = Prince(15, 'prince', 38, [cinderella.__dict__, fake_cinderella.__dict__, fake_cinderella2.__dict__])
+#
+#
+# 2)
+# Создать класс Rectangle:
+# -конструктор принимает две стороны x,y
+# -описать арифметические методы:
+#   + сума площадей двух экземпляров класса
+#   - разница площадей
+#   == или площади равны
+#   != не равны
+#   >, < меньше или больше
+#   при вызове метода len() подсчитывать сумму сторон
+
+# class Rectangle:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#         self.xy = self.x * self.y
+#
+#     def __add__(self, other, ):
+#         return self.xy + (other.x * other.y)
+#
+#     def __sub__(self, other):
+#         return self.xy - (other.x * other.y)
+#
+#     def __eq__(self, other):
+#         return self.xy == (other.x * other.y)
+#
+#     def __ne__(self, other):
+#         return self.xy != (other.x * other.y)
+#
+#     def __lt__(self, other):
+#         return self.xy < (other.x * other.y)
+#
+#     def __gt__(self, other):
+#         return self.xy > (other.x * other.y)
+#
+#     def __len__(self):
+#         return self.x + self.y
+#
+#
+# one = Rectangle(6, 7)
+# two = Rectangle(4, 5)
+# print(one!=two)
+# print(len(one))
+
